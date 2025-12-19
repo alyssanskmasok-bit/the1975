a5c00021cba4afbe9630324c75f502fc5316f6f5
alyssanskmasok-bit <alyssanskmasok@gmail.com>
2025-12-19 09:59:55 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+document.addEventListener("click", e => {
+  const box = e.target.closest(".video-wrap");
+  if (!box) return;
+
+  const id = box.dataset.youtubeId;
+
+  const iframe = document.createElement("iframe");
+  iframe.src = "https://www.youtube-nocookie.com/embed/" + id + "?autoplay=1";
+  iframe.width = "100%";
+  iframe.height = "450";
+  iframe.allow = "autoplay; encrypted-media";
+
+  box.innerHTML = "";
+  box.appendChild(iframe);
+});
