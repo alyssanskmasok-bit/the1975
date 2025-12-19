e4bf2775536b81cfc202fecd3be157c658d7ccad
alyssanskmasok-bit <alyssanskmasok@gmail.com>
2025-12-19 10:55:04 -0600

    script.js

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
 document.addEventListener("click", e => {
-  const box = e.target.closest(".video-wrap");
-  if (!box) return;
-
-  const id = box.dataset.youtubeId;
-
-  const iframe = document.createElement("iframe");
-  iframe.src = "https://www.youtube-nocookie.com/embed/" + id + "?autoplay=1";
-  iframe.width = "100%";
-  iframe.height = "450";
-  iframe.allow = "autoplay; encrypted-media";
-
-  box.innerHTML = "";
-  box.appendChild(iframe);
+ const box = e.target.closest(".video-wrap");
+ if (!box) return;
+ const id = box.dataset.youtubeId;
+ const iframe = document.createElement("iframe");
+ iframe.src = "https://www.youtube-nocookie.com/embed/" + id + "?autoplay=1";
+ iframe.width = "100%";
+ iframe.height = "450";
+ iframe.allow = "autoplay; encrypted-media";
+ iframe.style.border = "none";
+ box.innerHTML = "";
+ box.appendChild(iframe);
 });
